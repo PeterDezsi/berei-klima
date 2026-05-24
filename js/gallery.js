1e71dccdb1fa5a0dcac794587186e1589116007b
PeterDezsi <31287136+PeterDezsi@users.noreply.github.com>
2026-05-24 18:27:30 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,7 @@ async function loadGallery() {
   const { data, error } = await supabaseClient
     .storage
     .from('gallery')
-    .list('', {
-      limit: 100
-    });
+    .list('', { limit: 100 });
 
   if (error) {
     console.error(error);
@@ -13,24 +11,21 @@ async function loadGallery() {
   }
 
   const gallery = document.querySelector('.gallery');
-
   gallery.innerHTML = '';
 
   data.forEach(file => {
 
-    const imageUrl =
-      `${SUPABASE_URL}/storage/v1/object/public/gallery/${file.name}`;
+    const { data: urlData } = supabaseClient
+      .storage
+      .from('gallery')
+      .getPublicUrl(file.name);
 
     const img = document.createElement('img');
-
-    img.src = imageUrl;
-
+    img.src = urlData.publicUrl;
     img.alt = 'Referencia munka';
 
     gallery.appendChild(img);
-
   });
-
 }
 
 loadGallery();

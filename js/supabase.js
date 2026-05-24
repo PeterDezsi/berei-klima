42ffb2b95899d48946efebfef937311abac8f67f
PeterDezsi <31287136+PeterDezsi@users.noreply.github.com>
2026-05-24 17:52:52 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = 'IDE_JÖN_A_PROJECT_URL';
+const SUPABASE_ANON_KEY = 'IDE_JÖN_ANON_KEY';
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
